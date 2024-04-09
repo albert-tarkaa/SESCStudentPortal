94b0f90eec3c4fa95254a1f67a7f585e94958951
albert-tarkaa <tarkaa.albert@gmail.com>
2024-04-09 03:25:04 +0100

    Implemented the student course/enrollment endpoints

--- src/main/java/uk/ac/leedsbeckett/albertarkaa/studentportal/Model/CourseModel.java
+++ src/main/java/uk/ac/leedsbeckett/albertarkaa/studentportal/Model/CourseModel.java
@@ -7,6 +7,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -19,9 +22,18 @@ public class CourseModel {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private int id;
-    private String courseID;
+    @Column(nullable = false, unique = true, length = 50)
+    private String courseCode;
+    @Column(nullable = false, unique = true, length = 100)
     private String courseName;
+    private String courseDescription;
+    private double fee;
     @Column(updatable = false)
-    private LocalDate createdAt;
-    private LocalDate updatedAt;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @OneToMany(mappedBy = "course")
+    private List<StudentCourseModel> studentCourses = new ArrayList<>();
+
+
 }
