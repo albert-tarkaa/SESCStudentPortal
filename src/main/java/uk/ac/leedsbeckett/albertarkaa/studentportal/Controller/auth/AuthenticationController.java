0fa23d740a7825f9ed7e67a6829117622cd348f2
albert-tarkaa <tarkaa.albert@gmail.com>
2024-04-09 03:21:32 +0100

    Fixed the controller to use the Controller Response object

--- src/main/java/uk/ac/leedsbeckett/albertarkaa/studentportal/Controller/auth/AuthenticationController.java
+++ src/main/java/uk/ac/leedsbeckett/albertarkaa/studentportal/Controller/auth/AuthenticationController.java
@@ -1,12 +1,14 @@
 package uk.ac.leedsbeckett.albertarkaa.studentportal.Controller.auth;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.http.auth.InvalidCredentialsException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uk.ac.leedsbeckett.albertarkaa.studentportal.Service.AuthenticationService;
+import uk.ac.leedsbeckett.albertarkaa.studentportal.Utils.ControllerResponse;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -16,24 +18,19 @@ public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
     @PostMapping("/register")
-    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest registerRequest) {
-        return ResponseEntity.ok(authenticationService.register(registerRequest));
+    public ResponseEntity<ControllerResponse<AuthenticationResponse>> register(@RequestBody RegisterRequest registerRequest) {
+       return ResponseEntity.ok(authenticationService.register(registerRequest));
     }
 
     @PostMapping("/login")
-    public ResponseEntity<AuthenticationResponse> register(@RequestBody AuthenticationRequest authenticationRequest) {
+    public ResponseEntity<ControllerResponse<AuthenticationResponse>> register(@RequestBody AuthenticationRequest authenticationRequest) throws InvalidCredentialsException {
        return ResponseEntity.ok(authenticationService.login(authenticationRequest));
 
     }
 
-/*    @PostMapping("/logout")
-    public ResponseEntity<AuthenticationResponse> register(@RequestBody LogoutRequest logoutRequest) {
-
+    @PostMapping("/logout")
+    public ResponseEntity<ControllerResponse<AuthenticationResponse>> register(@RequestBody LogoutRequest logoutRequest) {
+    return ResponseEntity.ok(authenticationService.logout(logoutRequest));
     }
-
-    @PostMapping("/forgot-password")
-    public ResponseEntity<AuthenticationResponse> register(@RequestBody ForgotPasswordRequest forgotPasswordRequest) {
-
-    }*/
 
 }
