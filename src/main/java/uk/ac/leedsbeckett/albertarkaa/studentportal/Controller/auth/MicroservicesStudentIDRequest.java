68447b4c7c86d3732e09952ed6f468ea4d161cc9
albert-tarkaa <tarkaa.albert@gmail.com>
2024-04-17 23:13:36 +0100

    updated

---
+++ src/main/java/uk/ac/leedsbeckett/albertarkaa/studentportal/Controller/auth/MicroservicesStudentIDRequest.java
@@ -0,0 +1,14 @@
+package uk.ac.leedsbeckett.albertarkaa.studentportal.Controller.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MicroservicesAccountRequest {
+    private String StudentId;
+}
